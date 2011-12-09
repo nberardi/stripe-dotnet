b5ad26ec4ff1b9dd75f0793eeec50d8e3ff2e57a
Nick Berardi <nick@managedfusion.com>
2011-12-09 17:13:33 -0500

    all invoiceitems tests are working

---
+++ src/Invoices.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Linq;
+using RestSharp;
+using RestSharp.Extensions;
+using RestSharp.Validation;
+using Stripe.Models;
+
+namespace Stripe
+{
+	public partial class StripeClient
+	{
+		public InvoiceItemResponse CreateInvoiceItem(string customerId, decimal amount, string currency, string description = null)
+		{
+			Require.Argument("customerId", customerId);
+			Require.Argument("amount", amount);
+			Require.Argument("currency", currency);
+
+			var request = new RestRequest();
+			request.Method = Method.POST;
+			request.Resource = "invoiceitems";
+
+			request.AddParameter("customer", customerId);
+			request.AddParameter("amount", Convert.ToInt32(amount * 100));
+			request.AddParameter("currency", currency);
+			if (description.HasValue()) request.AddParameter("description", description);
+
+			return Execute<InvoiceItemResponse>(request);
+		}
+
+		public InvoiceItemResponse RetreiveInvoiceItem(string invoiceItemId)
+		{
+			Require.Argument("invoiceItemId", invoiceItemId);
+
+			var request = new RestRequest();
+			request.Resource = "invoiceitems/{invoiceItemId}";
+
+			request.AddUrlSegment("invoiceItemId", invoiceItemId);
+
+			return Execute<InvoiceItemResponse>(request);
+		}
+
+		public InvoiceItemResponse UpdateInvoiceItem(string invoiceItemId, decimal amount, string currency, string description = null)
+		{
+			Require.Argument("invoiceItemId", invoiceItemId);
+			Require.Argument("amount", amount);
+			Require.Argument("currency", currency);
+
+			var request = new RestRequest();
+			request.Method = Method.POST;
+			request.Resource = "invoiceitems/{invoiceItemId}";
+
+			request.AddUrlSegment("invoiceItemId", invoiceItemId);
+
+			request.AddParameter("amount", Convert.ToInt32(amount * 100));
+			request.AddParameter("currency", currency);
+			if (description.HasValue()) request.AddParameter("description", description);
+
+			return Execute<InvoiceItemResponse>(request);
+		}
+
+		public DeletedInvoiceItemResponse DeleteInvoiceItem(string invoiceItemId)
+		{
+			Require.Argument("invoiceItemId", invoiceItemId);
+
+			var request = new RestRequest();
+			request.Method = Method.DELETE;
+			request.Resource = "invoiceitems/{invoiceItemId}";
+
+			request.AddUrlSegment("invoiceItemId", invoiceItemId);
+
+			return Execute<DeletedInvoiceItemResponse>(request);
+		}
+
+		public ListResponse<CouponResponse> ListInvoiceItems(string customerId = null, int? count = null, int? offset = null)
+		{
+			var request = new RestRequest();
+			request.Resource = "invoiceitems";
+
+			if (customerId.HasValue()) request.AddParameter("customer", customerId);
+			if (count.HasValue) request.AddParameter("count", count.Value);
+			if (offset.HasValue) request.AddParameter("offset", offset.Value);
+
+			return Execute<ListResponse<CouponResponse>>(request);
+		}
+
+		public InvoiceResponse RetreiveInvoice(string invoiceId)
+		{
+			Require.Argument("invoiceId", invoiceId);
+
+			var request = new RestRequest();
+			request.Resource = "invoices/{invoiceId}";
+
+			request.AddUrlSegment("invoiceId", invoiceId);
+
+			return Execute<InvoiceResponse>(request);
+		}
+
+		public InvoiceResponse RetreiveCustomersUpcomingInvoice(string customerId)
+		{
+			Require.Argument("customerId", customerId);
+
+			var request = new RestRequest();
+			request.Resource = "invoices/upcoming";
+
+			request.AddParameter("customerId", customerId);
+
+			return Execute<InvoiceResponse>(request);
+		}
+
+		public ListResponse<InvoiceResponse> ListInvoices(string customerId = null, int? count = null, int? offset = null)
+		{
+			var request = new RestRequest();
+			request.Resource = "invoices";
+
+			if (customerId.HasValue()) request.AddParameter("customer", customerId);
+			if (count.HasValue) request.AddParameter("count", count.Value);
+			if (offset.HasValue) request.AddParameter("offset", offset.Value);
+
+			return Execute<ListResponse<InvoiceResponse>>(request);
+		}
+	}
+}
---
+++ src/Models/InvoiceResponse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stripe.Models
+{
+	public class InvoiceResponse : StripeBase
+	{
+		public long? Created { get; set; }
+		public int SubTotal { get; set; }
+		public int Total { get; set; }
+		public LinesWrapperResponse Lines { get; set; }
+		public string Id { get; set; }
+
+		public bool IsUpcoming { get { return Id == null; } }
+
+		public class LinesWrapperResponse : IEnumerable<InvoiceItemResponse>
+		{
+			public List<InvoiceItemResponse> InvoiceItems { get; set; }
+
+			#region IEnumerable<InvoiceItemResponse> Members
+
+			public IEnumerator<InvoiceItemResponse> GetEnumerator()
+			{
+				return InvoiceItems.GetEnumerator();
+			}
+
+			#endregion
+
+			#region IEnumerable Members
+
+			System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+			{
+				return InvoiceItems.GetEnumerator();
+			}
+
+			#endregion
+		}
+	}
+}
