4611a9dab34898c32488fe9ecd5dc7301f9890fd
Nick Berardi <nick@managedfusion.com>
2011-12-09 15:44:03 -0500

    all customer tests are working

--- src/Customers.cs
+++ src/Customers.cs
@@ -14,8 +14,8 @@ namespace Stripe
 			if (card != null)
 			{
 				Require.Argument("card[number]", card.Number);
-				Require.Argument("card[exp_month]", card.ExpirationMonth);
-				Require.Argument("card[exp_year]", card.ExpirationYear);
+				Require.Argument("card[exp_month]", card.ExpMonth);
+				Require.Argument("card[exp_year]", card.ExpYear);
 			}
 
 			var request = new RestRequest();
@@ -25,15 +25,15 @@ namespace Stripe
 			if (card != null)
 			{
 				request.AddParameter("card[number]", card.Number);
-				request.AddParameter("card[exp_month]", card.ExpirationMonth);
-				request.AddParameter("card[exp_year]", card.ExpirationYear);
-				if (card.Cvc.HasValue()) request.AddParameter("card[cvc]", card.ExpirationYear);
-				if (card.Name.HasValue()) request.AddParameter("card[name]", card.ExpirationYear);
-				if (card.AddressLine1.HasValue()) request.AddParameter("card[address_line1]", card.ExpirationYear);
-				if (card.AddressLine2.HasValue()) request.AddParameter("card[address_line2]", card.ExpirationYear);
-				if (card.AddressZip.HasValue()) request.AddParameter("card[address_zip]", card.ExpirationYear);
-				if (card.AddressState.HasValue()) request.AddParameter("card[address_state]", card.ExpirationYear);
-				if (card.AddressCountry.HasValue()) request.AddParameter("card[address_country]", card.ExpirationYear);
+				request.AddParameter("card[exp_month]", card.ExpMonth);
+				request.AddParameter("card[exp_year]", card.ExpYear);
+				if (card.Cvc.HasValue()) request.AddParameter("card[cvc]", card.ExpYear);
+				if (card.Name.HasValue()) request.AddParameter("card[name]", card.ExpYear);
+				if (card.AddressLine1.HasValue()) request.AddParameter("card[address_line1]", card.ExpYear);
+				if (card.AddressLine2.HasValue()) request.AddParameter("card[address_line2]", card.ExpYear);
+				if (card.AddressZip.HasValue()) request.AddParameter("card[address_zip]", card.ExpYear);
+				if (card.AddressState.HasValue()) request.AddParameter("card[address_state]", card.ExpYear);
+				if (card.AddressCountry.HasValue()) request.AddParameter("card[address_country]", card.ExpYear);
 			}
 			if (coupon.HasValue()) request.AddParameter("coupon", coupon);
 			if (email.HasValue()) request.AddParameter("email", email);
@@ -61,26 +61,28 @@ namespace Stripe
 			if (card != null)
 			{
 				Require.Argument("card[number]", card.Number);
-				Require.Argument("card[exp_month]", card.ExpirationMonth);
-				Require.Argument("card[exp_year]", card.ExpirationYear);
+				Require.Argument("card[exp_month]", card.ExpMonth);
+				Require.Argument("card[exp_year]", card.ExpYear);
 			}
 
 			var request = new RestRequest();
 			request.Method = Method.POST;
 			request.Resource = "customers/{customerId}";
 
+			request.AddUrlSegment("customerId", customerId);
+
 			if (card != null)
 			{
 				request.AddParameter("card[number]", card.Number);
-				request.AddParameter("card[exp_month]", card.ExpirationMonth);
-				request.AddParameter("card[exp_year]", card.ExpirationYear);
-				if (card.Cvc.HasValue()) request.AddParameter("card[cvc]", card.ExpirationYear);
-				if (card.Name.HasValue()) request.AddParameter("card[name]", card.ExpirationYear);
-				if (card.AddressLine1.HasValue()) request.AddParameter("card[address_line1]", card.ExpirationYear);
-				if (card.AddressLine2.HasValue()) request.AddParameter("card[address_line2]", card.ExpirationYear);
-				if (card.AddressZip.HasValue()) request.AddParameter("card[address_zip]", card.ExpirationYear);
-				if (card.AddressState.HasValue()) request.AddParameter("card[address_state]", card.ExpirationYear);
-				if (card.AddressCountry.HasValue()) request.AddParameter("card[address_country]", card.ExpirationYear);
+				request.AddParameter("card[exp_month]", card.ExpMonth);
+				request.AddParameter("card[exp_year]", card.ExpYear);
+				if (card.Cvc.HasValue()) request.AddParameter("card[cvc]", card.ExpYear);
+				if (card.Name.HasValue()) request.AddParameter("card[name]", card.ExpYear);
+				if (card.AddressLine1.HasValue()) request.AddParameter("card[address_line1]", card.ExpYear);
+				if (card.AddressLine2.HasValue()) request.AddParameter("card[address_line2]", card.ExpYear);
+				if (card.AddressZip.HasValue()) request.AddParameter("card[address_zip]", card.ExpYear);
+				if (card.AddressState.HasValue()) request.AddParameter("card[address_state]", card.ExpYear);
+				if (card.AddressCountry.HasValue()) request.AddParameter("card[address_country]", card.ExpYear);
 			}
 			if (coupon.HasValue()) request.AddParameter("coupon", coupon);
 			if (email.HasValue()) request.AddParameter("email", email);
@@ -89,7 +91,7 @@ namespace Stripe
 			return Execute<CustomerResponse>(request);
 		}
 
-		public CustomerResponse DeleteCustomer(string customerId)
+		public DeletedCustomerResponse DeleteCustomer(string customerId)
 		{
 			Require.Argument("customerId", customerId);
 
@@ -99,7 +101,7 @@ namespace Stripe
 
 			request.AddUrlSegment("customerId", customerId);
 
-			return Execute<CustomerResponse>(request);
+			return Execute<DeletedCustomerResponse>(request);
 		}
 
 		public CustomerListResponse ListCustomers(int count = 10, int offset = 0)
---
+++ test/Stripe.Tests/CustomerTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using Stripe.Models;
+
+namespace Stripe.Tests
+{
+	[TestFixture]
+	public class CustomerTests
+	{
+		private StripeClient _client;
+
+		private CreditCardRequest _card;
+		private string _email = "test@hoppio.com";
+
+		[SetUp]
+		public void Setup()
+		{
+			_card = new CreditCardRequest {
+				Number = "4111111111111111",
+				ExpMonth = 3,
+				ExpYear = 2015
+			};
+
+			_client = new StripeClient(Constants.ApiKey);
+		}
+
+		[Test]
+		public void CreateCustomer_Test()
+		{
+			var response = _client.CreateCustomer(_card, email: _email);
+
+			Assert.IsNotNull(response);
+			Assert.IsFalse(response.IsError);
+			Assert.IsNotNull(response.Id);
+		}
+
+		[Test]
+		public void RetrieveCustomer_Test()
+		{
+			var customer = _client.CreateCustomer(_card, email: _email);
+			var response = _client.RetrieveCustomer(customer.Id);
+
+			Assert.IsNotNull(response);
+			Assert.IsFalse(response.IsError);
+			Assert.AreEqual(customer.Id, response.Id);
+		}
+
+		[Test]
+		public void UpdateCustomer_Test()
+		{
+			var newCard = new CreditCardRequest {
+				Number = "378734493671000",
+				ExpMonth = 12,
+				ExpYear = 2016
+			};
+
+			var customer = _client.CreateCustomer(_card, email: _email);
+			var response = _client.UpdateCustomer(customer.Id, newCard);
+
+			Assert.IsNotNull(response);
+			Assert.IsFalse(response.IsError);
+			Assert.AreEqual(customer.Id, response.Id);
+			Assert.AreNotEqual(customer.ActiveCard.Last4, response.ActiveCard.Last4);
+		}
+
+		[Test]
+		public void DeleteCustomer_Test()
+		{
+			var customer = _client.CreateCustomer(_card, email: _email);
+			var response = _client.DeleteCustomer(customer.Id);
+
+			Assert.IsNotNull(response);
+			Assert.IsFalse(response.IsError);
+			Assert.IsTrue(response.Deleted);
+			Assert.AreEqual(customer.Id, response.Id);
+		}
+
+		[Test]
+		public void ListCharges_Test()
+		{
+			var response = _client.ListCustomers();
+
+			Assert.IsNotNull(response);
+			Assert.IsFalse(response.IsError);
+			Assert.IsNotNull(response.Data);
+			Assert.IsTrue(response.Data.Count > 0);
+		}
+	}
+}
