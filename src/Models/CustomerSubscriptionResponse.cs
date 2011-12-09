bf666e77ffea0977b15e82de31c509fbaad736c9
Nick Berardi <nick@managedfusion.com>
2011-12-09 16:27:54 -0500

    all subscriptions tests are working

---
+++ src/Models/CustomerSubscriptionResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Stripe.Models
+{
+	public class CustomerSubscriptionResponse : StripeBase
+	{
+		public long CurrentPeriodEnd { get; set; }
+		public string Status { get; set; }
+		public CustomerSubscriptionPlanResponse Plan { get; set; }
+		public long CurrentPeriodStart { get; set; }
+		public long Start { get; set; }
+		public long TrialStart { get; set; }
+		public long TrialEnd { get; set; }
+		public long? CanceledAt { get; set; }
+		public long? EndedAt { get; set; }
+		public string Customer { get; set; }
+	}
+}
---
+++ test/Stripe.Tests/SubscriptionTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using Stripe.Models;
+
+namespace Stripe.Tests
+{
+	[TestFixture]
+	public class SubscriptionTests
+	{
+		private PlanResponse _plan;
+		private CustomerResponse _customer;
+
+		private StripeClient _client;
+
+		[SetUp]
+		public void Setup()
+		{
+			_client = new StripeClient(Constants.ApiKey);
+
+			var id = Guid.NewGuid().ToString();
+			var card = new CreditCardRequest {
+				Number = "4111111111111111",
+				ExpMonth = 3,
+				ExpYear = 2015
+			};
+
+			_plan = _client.CreatePlan(id, 400M, "usd", PlanFrequency.Month, id);
+			_customer = _client.CreateCustomer(card);
+		}
+
+		[Test]
+		public void UpdateCustomersSubscription_Test()
+		{
+			var response = _client.UpdateCustomersSubscription(_customer.Id, _plan.Id);
+
+			Assert.IsNotNull(response);
+			Assert.IsFalse(response.IsError);
+		}
+
+		[Test]
+		public void CancelCustomersSubscription_Test()
+		{
+			_client.UpdateCustomersSubscription(_customer.Id, _plan.Id);
+			var response = _client.CancelCustomersSubscription(_customer.Id);
+
+			Assert.IsNotNull(response);
+			Assert.IsFalse(response.IsError);
+			Assert.IsNotNull(response.CanceledAt);
+			Assert.IsNotNull(response.EndedAt);
+		}
+	}
+}
