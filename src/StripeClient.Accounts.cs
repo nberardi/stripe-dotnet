f441c24ff37f595c4d4ef257580dfcafa463299d
Bates Westmoreland <rbwestmoreland@gmail.com>
2013-01-13 22:26:47 -0500

    added the ability to retrieve an account
    - added RetrieveAccount method
    - added RetrieveAccount unit test

---
+++ src/StripeClient.Accounts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using RestSharp;
+using RestSharp.Validation;
+
+
+namespace Stripe
+{
+	public partial class StripeClient
+	{
+		public StripeObject RetrieveAccount()
+		{
+			var request = new RestRequest();
+			request.Resource = "account";
+
+			return ExecuteObject(request);
+		}
+	}
+}
