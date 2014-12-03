6d54f9ce74a0ce666fc15237e856abd232c1f0a5
Paul Irwin <paulirwin@gmail.com>
2014-12-03 17:17:32 -0500

    Change to build.cmd to use relative nuget path

--- build.cmd
+++ build.cmd
@@ -7,5 +7,5 @@ if "%config%" == "" (
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild .\src\Stripe.csproj /p:Configuration="%config%" /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
 if "%config%" == "Release" (
-	nuget pack .\src\Stripe.csproj -Build -Symbols -Properties Configuration=Release
+	.nuget\nuget pack .\src\Stripe.csproj -Build -Symbols -Properties Configuration=Release
 )
