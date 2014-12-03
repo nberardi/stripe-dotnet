a15adb421f2bc57e0f9430b2b97d52cc4483d091
Paul Irwin <paulirwin@gmail.com>
2014-12-03 17:09:55 -0500

    Prepare for 1.12.0-beta1 release
    Upgrades RestSharp to latest version

--- src/StripeClient.cs
+++ src/StripeClient.cs
@@ -7,83 +7,85 @@ using RestSharp;
 
 namespace Stripe
 {
-	/// <seealso href="https://stripe.com/docs/api"/>
-	public partial class StripeClient
-	{
-		public string ApiVersion { get; set; }
-		public string ApiEndpoint { get; set; }
-		public string ApiKey { get; private set; }
+    /// <seealso href="https://stripe.com/docs/api"/>
+    public partial class StripeClient
+    {
+        public string ApiVersion { get; set; }
+        public string ApiEndpoint { get; set; }
+        public string ApiKey { get; private set; }
 
-		private RestClient _client;
+        private RestClient _client;
 
-		public StripeClient(string apiKey)
-		{
-			ApiVersion = "v1";
-			ApiEndpoint = "https://api.stripe.com/";
-			ApiKey = apiKey;
+        public StripeClient(string apiKey)
+        {
+            ApiVersion = "v1";
+            ApiEndpoint = "https://api.stripe.com/";
+            ApiKey = apiKey;
 
-			// silverlight friendly way to get current version
-			var assembly = Assembly.GetExecutingAssembly();
-			AssemblyName assemblyName = new AssemblyName(assembly.FullName);
-			var version = assemblyName.Version;
+            // silverlight friendly way to get current version
+            var assembly = Assembly.GetExecutingAssembly();
+            AssemblyName assemblyName = new AssemblyName(assembly.FullName);
+            var version = assemblyName.Version;
 
-			_client = new RestClient();
-			_client.UserAgent = "stripe-dotnet/" + version;
-			_client.Authenticator = new StripeAuthenticator(apiKey);
-			_client.BaseUrl = String.Format("{0}{1}", ApiEndpoint, ApiVersion);
-		}
+            _client = new RestClient();
+            _client.UserAgent = "stripe-dotnet/" + version;
+            _client.Authenticator = new StripeAuthenticator(apiKey);
+            _client.BaseUrl = new Uri(String.Format("{0}{1}", ApiEndpoint, ApiVersion));
+        }
 
-		/// <summary>
-		/// Execute a manual REST request
-		/// </summary>
-		/// <typeparam name="T">The type of object to create and populate with the returned data.</typeparam>
-		/// <param name="request">The RestRequest to execute (will use client credentials)</param>
-		public StripeObject ExecuteObject(RestRequest request)
-		{
-			request.OnBeforeDeserialization = (resp) => {
-				// for individual resources when there's an error to make
-				// sure that RestException props are populated
-				if (((int)resp.StatusCode) >= 400)
-					request.RootElement = "";
-			};
+        /// <summary>
+        /// Execute a manual REST request
+        /// </summary>
+        /// <typeparam name="T">The type of object to create and populate with the returned data.</typeparam>
+        /// <param name="request">The RestRequest to execute (will use client credentials)</param>
+        public StripeObject ExecuteObject(RestRequest request)
+        {
+            request.OnBeforeDeserialization = (resp) =>
+            {
+                // for individual resources when there's an error to make
+                // sure that RestException props are populated
+                if (((int)resp.StatusCode) >= 400)
+                    request.RootElement = "";
+            };
 
-			var response = _client.Execute(request);
-			var json = Deserialize(response.Content);
-			var obj = new StripeObject();
-			obj.SetModel(json);
+            var response = _client.Execute(request);
+            var json = Deserialize(response.Content);
+            var obj = new StripeObject();
+            obj.SetModel(json);
 
-			return obj;
-		}
+            return obj;
+        }
 
-		/// <summary>
-		/// Execute a manual REST request
-		/// </summary>
-		/// <typeparam name="T">The type of object to create and populate with the returned data.</typeparam>
-		/// <param name="request">The RestRequest to execute (will use client credentials)</param>
-		public StripeArray ExecuteArray(RestRequest request)
-		{
-			request.OnBeforeDeserialization = (resp) => {
-				// for individual resources when there's an error to make
-				// sure that RestException props are populated
-				if (((int)resp.StatusCode) >= 400)
-					request.RootElement = "";
-			};
+        /// <summary>
+        /// Execute a manual REST request
+        /// </summary>
+        /// <typeparam name="T">The type of object to create and populate with the returned data.</typeparam>
+        /// <param name="request">The RestRequest to execute (will use client credentials)</param>
+        public StripeArray ExecuteArray(RestRequest request)
+        {
+            request.OnBeforeDeserialization = (resp) =>
+            {
+                // for individual resources when there's an error to make
+                // sure that RestException props are populated
+                if (((int)resp.StatusCode) >= 400)
+                    request.RootElement = "";
+            };
 
-			var response = _client.Execute(request);
-			var json = Deserialize(response.Content);
-			var obj = new StripeArray();
-			obj.SetModel(json);
+            var response = _client.Execute(request);
+            var json = Deserialize(response.Content);
+            var obj = new StripeArray();
+            obj.SetModel(json);
 
-			return obj;
-		}
+            return obj;
+        }
 
-		private IDictionary<string, object> Deserialize(string input)
-		{
-			if (String.IsNullOrEmpty(input))
-				return null;
+        private IDictionary<string, object> Deserialize(string input)
+        {
+            if (String.IsNullOrEmpty(input))
+                return null;
 
-			var serializer = new JavaScriptSerializer();
-			return serializer.Deserialize<IDictionary<string, object>>(input);
-		}
-	}
+            var serializer = new JavaScriptSerializer();
+            return serializer.Deserialize<IDictionary<string, object>>(input);
+        }
+    }
 }
